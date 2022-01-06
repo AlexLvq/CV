cbe33827b74bd8cd104cbccf399fd528013c061e
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-06 16:12:22 +0100

    CV

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -37,31 +37,3 @@ $(".trois").animate({
 $(".moitie").animate({
     width: "50%"
 }, 2500);
-
-$(function() {
-    
-    var $meters = $(".progress progress-striped active > span");
-    var $section = $('#third');
-    var $queue = $({});
-    
-    function loadDaBars() {
-				$(".progress progress-striped active > span").each(function() {
-					$(this)
-						.data("origWidth", $(this).width())
-						.width(0)
-						.animate({
-							width: $(this).data("origWidth")
-						}, 1200);
-				});
-    }
-    
-    $(document).bind('scroll', function(ev) {
-        var scrollOffset = $(document).scrollTop();
-        var containerOffset = $section.offset().top - window.innerHeight;
-        if (scrollOffset > containerOffset) {
-            loadDaBars();
-            $(document).unbind('scroll');
-        }
-    });
-    
-});
