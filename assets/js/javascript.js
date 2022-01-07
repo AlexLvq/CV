42f4fb021a5259b80026702e16a19c4c260ef603
Alex Lvq <alexandrelvqq@gmail.com>
2022-01-07 16:24:45 +0100

    CV

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,17 +9,8 @@ $(window).scroll(function () {
     else
         $('#nav1').slideUp(450);
 });
-// NavBar disparait --------------------------------------------------
-$(function () {
-    $('#navaccueil').show();
-});
-$(window).scroll(function () {
-    posScroll = $(document).scrollTop();
-    if (posScroll >= 0)
-        $('#navaccueil').slideDown(450);
-    else
-        $('#navaccueil').slideUp(450);
-});
+
+
 
 
 // Barre annimée --------------------------------------------------
@@ -40,17 +31,6 @@ $(".moitie").animate({
 
 
 // hover + active sur les mots de la NavBar --------------------------------------------------
-window.addEventListener('scroll', function () {
-    section.forEach(item => {
-        if (scrollY > (item.offsetTop + (window.innerHeight / 3)) && scrollY <= (item.offsetTop + item.clientHeight + (window.innerHeight / 3))) {
 
-            //d'abord on supprime la classe active sur tous
-            navbar_a.forEach(anchor => {
-                anchor.classList.remove('active');
-            })
 
-            // on met ensuite la classe active sur le lien qui a l'id = link-(id de l'item selectionné)
-            document.querySelector('#experience' + item.getAttribute('id')).classList.add('active');
-        }
-    });
-});
+
